dc04c1c66afcb635ae045a93418204de080eec8d
Bruno de Souza Rocha <brunodotcom@outlook.com>
2019-05-15 15:58:58 -0300

    Adding save post feature into BlogPost

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Serializer;
+use App\Entity\BlogPost;
 
 /**
  * @Route("/blog")
@@ -35,7 +37,7 @@ class BlogController extends AbstractController
      *
      * @return void
      * 
-     * @Route("/{page}", name="blog_list", defaults={"page" : 5})
+     * @Route("/{page}", name="blog_list", defaults={"page" : 5}, requirements={"page"="\d+"})
      */
     public function list($page = 1, Request $request)
     {
@@ -56,7 +58,7 @@ class BlogController extends AbstractController
      *
      * @return void
      * 
-     * @Route("/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"})
      */
     public function post($id)
     {
@@ -69,13 +71,33 @@ class BlogController extends AbstractController
      *
      * @return void
      * 
-     * @Route("/{slug}", name="blog_by_slug")
+     * @Route("/post/{slug}", name="blog_by_slug")
      */
     public function postBySlug($slug)
     {
         return $this->json(
             self::POSTS[array_search($slug, array_column(self::POSTS, "slug"))]
         );
+    }
+    
+    /**
+     *
+     * @return void
+     * 
+     * @Route("/add", name="blog_add", methods={"POST"})
+     */
+    public function add(Request $request)
+    {
+        /** @var Serializer $serializer */
+        $serializer = $this->get("serializer");
+
+        $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, "json");
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($blogPost);
+        $em->flush();
+
+        return $this->json($blogPost);
     }
 
 }
