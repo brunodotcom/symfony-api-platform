65aeae55dc817cecc8d41e662a22f15c7a6af81f
Bruno de Souza Rocha <brunodotcom@outlook.com>
2019-05-16 14:32:23 -0300

    Setting DataFixtures for test

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,29 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\BlogPost;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $blogPost = new BlogPost();
+        $blogPost->setTitle("A first post!");
+        $blogPost->setPublished(new \DateTime("2019-05-16 12:00:00"));
+        $blogPost->setAuthor("Bruno Rocha");
+        $blogPost->setContent("Post Text");
+        $blogPost->setSlug("a-first-post");
+
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost();
+        $blogPost->setTitle("A second post!");
+        $blogPost->setPublished(new \DateTime("2019-05-16 12:00:00"));
+        $blogPost->setAuthor("Bruno Rocha");
+        $blogPost->setContent("Post Text");
+        $blogPost->setSlug("a-second-post");
+
+        $manager->persist($blogPost);
 
         $manager->flush();
     }
