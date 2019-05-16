e4e9b1ae8a3878a18fd187ee05c27348c7d44180
Bruno de Souza Rocha <brunodotcom@outlook.com>
2019-05-16 17:25:17 -0300

    Fixing BlogPost fixtures and adding User to it

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,15 +5,29 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\BlogPost;
+use App\Entity\User;
+use App\Entity\Comment;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder) {
+        $this->passwordEncoder = $passwordEncoder;
+    }
     public function load(ObjectManager $manager)
     {
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+    }
+
+    public function loadBlogPosts(ObjectManager $manager)
+    {
+        $user = $this->getReference("user_admin");
+
         $blogPost = new BlogPost();
         $blogPost->setTitle("A first post!");
         $blogPost->setPublished(new \DateTime("2019-05-16 12:00:00"));
-        $blogPost->setAuthor("Bruno Rocha");
+        $blogPost->setAuthor($user);
         $blogPost->setContent("Post Text");
         $blogPost->setSlug("a-first-post");
 
@@ -22,12 +36,36 @@ class AppFixtures extends Fixture
         $blogPost = new BlogPost();
         $blogPost->setTitle("A second post!");
         $blogPost->setPublished(new \DateTime("2019-05-16 12:00:00"));
-        $blogPost->setAuthor("Bruno Rocha");
+        $blogPost->setAuthor($user);
         $blogPost->setContent("Post Text");
         $blogPost->setSlug("a-second-post");
 
         $manager->persist($blogPost);
 
+        $manager->flush();
+    }
+
+    public function loadComments(ObjectManager $manager)
+    {
+        
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername("admin");
+        $user->setEmail("brunodotcom@outlook.com");
+        $user->setName("Bruno Rocha");
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword(
+                $user,
+                "secret123#"
+            )
+        );
+
+        $this->addReference("user_admin", $user);
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
