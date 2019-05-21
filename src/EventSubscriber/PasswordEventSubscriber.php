ae839a60e965e1093170983f859c3478070714d0
Bruno de Souza Rocha <brunodotcom@outlook.com>
2019-05-21 09:28:11 -0300

    Adding Password Hash interface to API

---
+++ src/EventSubscriber/PasswordEventSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\EventSubscriber;
+
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+
+class PasswordEventSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var PasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(PasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ["hashPassword", EventPriorities::PRE_WRITE]
+        ];
+    }
+
+    public function hashPassword(GetResponseForControllerResultEvent $event)
+    {
+        $user = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if (!$user instanceof User || Request::METHOD_POST != $method) {
+            return ;
+        }
+
+        // It is an User, we need to hash the password here
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword($user, $user->getPassword())
+        );
+    }
+
+}
