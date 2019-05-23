10fc9e8ce4aec6ed6af84b2e28c4c76681ee1b75
Bruno de Souza Rocha <brunodotcom@outlook.com>
2019-05-23 10:45:21 -0300

    Adding now datetime as published Date in BlogPost and Comment entities

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -5,6 +5,9 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ApiResource(
@@ -19,7 +22,10 @@ use Symfony\Component\Security\Core\User\UserInterface;
  *          "post" = {
  *              "access_control"="is_granted('IS_AUTHENTICATED_FULLY')"
  *          }
- *      }
+ *      },
+ *     denormalizationContext={
+ *          "groups"={"post"}
+ *     }
  * )
  * @ORM\Entity(repositoryClass="App\Repository\CommentRepository")
  */
@@ -34,6 +40,9 @@ class Comment implements AuthoredEntityInterface, PublishedDateEntityInterface
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     * @Assert\Length(min=5, max=3000)
+     * @Groups({"post"})
      */
     private $content;
 
